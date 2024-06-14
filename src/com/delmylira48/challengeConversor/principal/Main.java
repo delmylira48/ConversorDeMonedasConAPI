82c7a5dc012ad18697f693b61b382b961b6303d4
delmy21lk <delmy.lira48@gmail.com>
2024-06-14 01:17:13 -0600

    Agregado la llamada a la api

--- src/com/delmylira48/challengeConversor/principal/Main.java
+++ src/com/delmylira48/challengeConversor/principal/Main.java
@@ -1,2 +1,19 @@
-package com.delmylira48.challengeConversor.principal;public class Main {
+package com.delmylira48.challengeConversor.principal;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sea bienvenido/a al conversor de monedas\n" +
+                "1) Dolar --> Peso argentino\n" +
+                "2) Peso argentino --> Dolar\n" +
+                "3) Dolar --> Real brasileño\n" +
+                "4) Real brasileño --> Dolar\n" +
+                "5) Dolar --> Peso colombiano\n" +
+                "6) Peso colombiano --> Dolar\n" +
+                "7) Salir\n" +
+                "Elija una opción");
+
+        System.out.println("Ingrese el valor que desea convertir");
+
+        System.out.println("El valor x [moneda] corresponde al valor final x [moneda]");
+    }
 }
