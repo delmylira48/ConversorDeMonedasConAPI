dd035d67eae9c3c3ceeb622e12004e69e0d1cbc9
delmy21lk <delmy.lira48@gmail.com>
2024-06-14 01:17:13 -0600

    Agregado clase records DTO

---
+++ src/com/delmylira48/challengeConversor/DTO/MonedaDTO.java
@@ -0,0 +1,6 @@
+package com.delmylira48.challengeConversor.DTO;
+
+import java.util.Map;
+
+public record MonedaDTO(Map<String, Double> conversion_rates) {
+}
---
+++ src/com/delmylira48/challengeConversor/domain/LlamadaAPI.java
@@ -0,0 +1,35 @@
+package com.delmylira48.challengeConversor.domain;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class LlamadaAPI {
+    private String moneda;
+
+    public LlamadaAPI(String moneda) {
+        this.moneda=moneda;
+    }
+
+    public void llamada(){
+        URI url = URI.create("https://v6.exchangerate-api.com/v6/9e1edbf9cbb6d9c3c3f43e7f/latest/"+ moneda);
+        String contenidoJson;
+
+        try{
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(url)
+                    .build();
+
+            HttpResponse<String> httpResponse = httpClient
+                    .send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            contenidoJson = httpResponse.body();
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
